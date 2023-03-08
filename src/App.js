fded2082a5c1295be9669f0a30ac15a8af0aa228
Brian N <0xb3rlin@gmail.com>
2023-03-08 11:27:57 -0600

    file setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App bg-teal-800 min-h-screen text-yellow-100'>App</div>
+  );
+}
+
+export default App;
